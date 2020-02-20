6dbbed29c550ddc12f0fde2292389b5c3b23f785
Bartłomiej Sługocki <0@bartan0.pl>
2020-02-20 21:15:45 +0100

    Added EJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path')
 const express = require('express')
 
 const config = require('./config')
@@ -5,8 +6,17 @@ const config = require('./config')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', resolve(__dirname, 'views'))
+
 app
 	.get('/', (req, res) => {
-		res.end('OK')
+		res.redirect('/example')
 	})
+	.get('/:param', (req, res) => {
+		res.render('index', {
+			param: req.params.param
+		})
+	})
+
 	.listen(config.PORT)
