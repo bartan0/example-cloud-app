87a454b1a015d1bc02b15fc8eb6aa774f3816aee
Bartłomiej Sługocki <0@bartan0.pl>
2020-03-21 19:49:12 +0100

    Added support for email test mode

--- src/mail.js
+++ src/mail.js
@@ -2,7 +2,8 @@ const SendGrid = require('@sendgrid/mail')
 
 const MailService = {
 	KEY: process.env['MAIL_SENDGRID_KEY'],
-	SENDER: process.env['MAIL_SENDER'] || 'rss@example.com'
+	SENDER: process.env['MAIL_SENDER'] || 'rss@example.com',
+	TEST: !!process.env['MAIL_TEST_MODE']
 }
 
 MailService._init = function () {
@@ -16,7 +17,8 @@ MailService.send = function (receipient, subject, content) {
 		from: this.SENDER,
 		to: receipient,
 		subject,
-		html: content
+		html: content,
+		mail_settings: { sandbox_mode: { enable: this.TEST } }
 	})
 }
 
